bbf516de46a17f6b93c77e5628bc23848d7e4219
Niklas Bülow <github@insightmind.net>
2017-10-25 14:48:34 +0200

    added settingslist

---
+++ HyzeCalendarForWWDC/SettingsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsListTableViewCell.swift
+//  HyzeCalendarForWWDC
+//
+//  Created by Niklas Bülow on 25.10.17.
+//  Copyright © 2017 Niklas Bülow. All rights reserved.
+//
+
+import UIKit
+
+class SettingsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
